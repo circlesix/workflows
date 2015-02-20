4ca9963cc9adb53bfe22c5ba75067afe7a36268d
circle six <theologiae@gmail.com>
2015-02-19 18:44:57 -0700

    first commit

---
+++ components/coffee/tagline.coffee
@@ -0,0 +1,4 @@
+$ = require 'jquery'
+do fill = (item = 'The most creative minds in Art') ->
+$('.tagline').append "#{item}"
+fill
---
+++ components/scripts/templates.js
@@ -0,0 +1,8 @@
+$(function() {
+	var Mustache = require('mustache');
+	$.getJSON('js/data.json', function(data) {
+		var template = $('#speakerstpl').html();
+		var html = Mustache.to_html(template, data);
+		$('#speakers').html(html);
+	}); //getJSON
+}); //function
