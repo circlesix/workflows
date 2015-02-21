df0f66cfbb975c4a32b27fc57ce20dd6bfcce2c3
circle six <theologiae@gmail.com>
2015-02-20 21:08:38 -0700

    added the sass builds to gulp, commented out the browserify because of unresolved error in compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
+	//browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 
 var coffeeSource = ['components/coffee/tagline.coffee'];
@@ -10,6 +12,7 @@ var jsSources = [
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss']
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSource)
@@ -21,5 +24,17 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
 	gulp.src(jsSources)
 	.pipe(concat('script.js'))
+	//.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+	.pipe(compass({
+		sass: 'components/sass',
+		image: 'builds/development/images',
+		style: 'expanded'
+	}))
+	.on('error', gutil.log)
+	.pipe(gulp.dest('builds/development/css'))
 });
